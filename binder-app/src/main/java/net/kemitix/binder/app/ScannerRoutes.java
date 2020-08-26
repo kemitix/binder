12250d85dbc0218fca2cffa4c856045f1adf2e84
Paul Campbell <pcampbell@kemitix.net>
2020-08-26 20:44:43 +0100

    app: add empty ScannerRoutes

---
+++ binder-app/src/main/java/net/kemitix/binder/app/ScannerRoutes.java
@@ -0,0 +1,14 @@
+package net.kemitix.binder.app;
+
+import org.apache.camel.builder.RouteBuilder;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ScannerRoutes
+        extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
