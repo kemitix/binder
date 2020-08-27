a2b0514c14d8a0db5954dded80d6cc667e6c198c
Paul Campbell <pcampbell@kemitix.net>
2020-08-27 19:52:09 +0100

    Move injections of BinderConfig to ManuscriptLoader

--- binder-app/src/main/java/net/kemitix/binder/app/BinderApp.java
+++ binder-app/src/main/java/net/kemitix/binder/app/BinderApp.java
@@ -9,21 +9,17 @@ import javax.inject.Inject;
 @ApplicationScoped
 public class BinderApp {
 
-    private final BinderConfig binderConfig;
     private final Manuscript manuscript;
 
     @Inject
     public BinderApp(
-            BinderConfig binderConfig,
             Manuscript manuscript
     ) {
-        this.binderConfig = binderConfig;
         this.manuscript = manuscript;
     }
 
     public void run(String[] args) {
         log.info("Binder - Starting");
-        log.info("Loading " + binderConfig.getScanDirectory());
         manuscript.getPrelude().forEach(prelude -> log.info("Prelude: " + prelude.getName()));
         manuscript.getContents().forEach(section -> log.info("Prelude: " + section.getName()));
         manuscript.getCoda().forEach(coda -> log.info("Prelude: " + coda.getName()));
--- binder-app/src/main/java/net/kemitix/binder/app/ManuscriptLoader.java
+++ binder-app/src/main/java/net/kemitix/binder/app/ManuscriptLoader.java
@@ -1,13 +1,17 @@
 package net.kemitix.binder.app;
 
+import lombok.extern.java.Log;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 
+@Log
 @ApplicationScoped
 public class ManuscriptLoader {
 
     @Produces
-    Manuscript manuscript() {
+    Manuscript manuscript(BinderConfig binderConfig) {
+        log.info("Loading Manuscript from " + binderConfig.getScanDirectory());
         return new Manuscript();
     }
 
