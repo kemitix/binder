3a08a3835ddbea6fdfa354aea8259b3a941a0ad5
Paul Campbell <pcampbell@kemitix.net>
2020-09-27 17:36:11 +0100

    Add copyright page

--- binder-app/src/main/java/net/kemitix/binder/app/TemplateEngine.java
+++ binder-app/src/main/java/net/kemitix/binder/app/TemplateEngine.java
@@ -8,10 +8,12 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class TemplateEngine {
 
+    private static final String NON_BREAKING_SPACE = "\u00A0";
     private final VelocityEngine velocityEngine;
 
     @Inject
@@ -28,9 +30,26 @@ public class TemplateEngine {
         context.put("m", manuscript.getMetadata());
         context.put("c", manuscript.getContents());
         context.put("s", section);
+        context.put("copyrights", copyrights(manuscript));
         Writer writer = new StringWriter();
         velocityEngine.evaluate(context, writer, "", templateBody);
         return writer.toString();
+    }
+
+    private String copyrights(Manuscript manuscript) {
+        return manuscript.getContents().stream()
+                .filter(section -> "story".equals(section.getType()))
+                .map(section -> "%s ©%s by%s%s".formatted(
+                        section.getTitle(),
+                        section.getCopyright(),
+                        NON_BREAKING_SPACE,
+                        unBreakable(section.getAuthor())
+                ))
+                .collect(Collectors.joining("<br/>"));
+    }
+
+    private String unBreakable(String s) {
+        return s.replaceAll(" ", NON_BREAKING_SPACE);
     }
 
 }
