376c072f67d6dba7f77ceb5ce10a1bbe584fc69a
Paul Campbell <pcampbell@kemitix.net>
2020-10-12 08:04:02 +0100

    docx: refactoring

--- binder-app/src/main/java/net/kemitix/binder/app/docx/DocxBook.java
+++ binder-app/src/main/java/net/kemitix/binder/app/docx/DocxBook.java
@@ -1,33 +1,70 @@
 package net.kemitix.binder.app.docx;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.extern.java.Log;
+import org.docx4j.convert.in.xhtml.XHTMLImporterImpl;
+import org.docx4j.jaxb.Context;
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.exceptions.InvalidFormatException;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.openpackaging.parts.WordprocessingML.NumberingDefinitionsPart;
+import org.docx4j.wml.RFonts;
 
-import javax.enterprise.inject.Vetoed;
-import java.util.ArrayList;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.xml.bind.JAXBException;
+import java.io.File;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Log
-@Vetoed
-@NoArgsConstructor
-@AllArgsConstructor
+@ApplicationScoped
 public class DocxBook {
 
-    private String language;
-    private String id;
-    private String title;
-    private String editor;
-    private final List<DocxContent> content = new ArrayList<>();
+    private final List<DocxContent> contents;
 
-    public void writeToFile(String file) {
-        //TODO write self to file
-        log.info("TODO - write " + title + " to: " + file);
-        content.forEach(docxContent -> docxContent.save(file));
+    @Inject
+    public DocxBook(List<DocxContent> contents) {
+        this.contents = contents;
     }
 
-    public void addContent(DocxContent content) {
-        this.content.add(content);
+    public void writeToFile(String file) {
+        log.info("Write: " + file);
+        configureFontMapping();
+        try {
+            createMainDocument().save(new File(file));
+        } catch (Docx4JException | JAXBException e) {
+            throw new RuntimeException(
+                    "Error saving file: %s".formatted(file), e);
+        }
+    }
+
+    private void configureFontMapping() {
+        RFonts rfonts = Context.getWmlObjectFactory().createRFonts();
+        rfonts.setAscii("Century Gothic");
+        XHTMLImporterImpl.addFontMapping("Century Gothic", rfonts);
+    }
+
+    private WordprocessingMLPackage createMainDocument()
+            throws InvalidFormatException, JAXBException {
+        var wordMLPackage = WordprocessingMLPackage.createPackage();
+        var mainDocument = wordMLPackage.getMainDocumentPart();
+        definePartNumbering(mainDocument);
+        mainDocument.getContent().addAll(getContents());
+        return wordMLPackage;
+    }
+
+    private void definePartNumbering(MainDocumentPart mainDocument)
+            throws InvalidFormatException, JAXBException {
+        var numberingDefinitionsPart = new NumberingDefinitionsPart();
+        numberingDefinitionsPart.unmarshalDefaultNumbering();
+        mainDocument.addTargetPart(numberingDefinitionsPart);
+    }
+
+    private List<?> getContents() {
+        return contents.stream()
+                .flatMap(docxContent -> docxContent.getContents().stream())
+                .collect(Collectors.toList());
     }
 
 }
--- binder-app/src/main/java/net/kemitix/binder/app/docx/DocxFactory.java
+++ binder-app/src/main/java/net/kemitix/binder/app/docx/DocxFactory.java
@@ -3,7 +3,6 @@ package net.kemitix.binder.app.docx;
 import lombok.extern.java.Log;
 import net.kemitix.binder.app.BinderConfig;
 import net.kemitix.binder.app.HtmlManuscript;
-import net.kemitix.binder.app.Metadata;
 import net.kemitix.binder.app.Section;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -12,6 +11,7 @@ import javax.inject.Inject;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Log
 @ApplicationScoped
@@ -19,29 +19,26 @@ public class DocxFactory {
 
     private final BinderConfig binderConfig;
     private final HtmlManuscript htmlManuscript;
-    private final DocxContentFactory docxContextFactory;
+    private final DocxContentFactory docxContentFactory;
 
     @Inject
     public DocxFactory(
             BinderConfig binderConfig,
             HtmlManuscript htmlManuscript,
-            DocxContentFactory docxContextFactory
+            DocxContentFactory docxContentFactory
     ) {
         this.binderConfig = binderConfig;
         this.htmlManuscript = htmlManuscript;
-        this.docxContextFactory = docxContextFactory;
+        this.docxContentFactory = docxContentFactory;
     }
 
     @Produces
     @ApplicationScoped
-    public DocxBook create() {
-        Metadata metadata = htmlManuscript.getMetadata();
-        DocxBook docx = createDocxBook(metadata);
-        htmlManuscript.getHtmlSections().entrySet().stream()
+    public List<DocxContent> create() {
+        return htmlManuscript.getHtmlSections().entrySet().stream()
                 .filter(includeInDocx(htmlManuscript.getContents()))
-                .map(e -> docxContextFactory.create(e.getKey(), e.getValue()))
-                .forEach(docx::addContent);
-        return docx;
+                .map(e -> docxContentFactory.create(e.getKey(), e.getValue()))
+                .collect(Collectors.toList());
     }
 
     private Predicate<Map.Entry<String, String>> includeInDocx(List<Section> contents) {
@@ -51,15 +48,4 @@ public class DocxFactory {
                 .anyMatch(name -> name.equals(entry.getKey()));
     }
 
-    private DocxBook createDocxBook(Metadata metadata) {
-        String language = metadata.getLanguage();
-        log.info("Language: " + language);
-        String id = metadata.getId();
-        log.info("Id: " + id);
-        String title = metadata.getTitle();
-        log.info("Title: " + title);
-        String editor = metadata.getEditor();
-        log.info("Editor: " + editor);
-        return new DocxBook(language, id, title, editor);
-    }
 }
