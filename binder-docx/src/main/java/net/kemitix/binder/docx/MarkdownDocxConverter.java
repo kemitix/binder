d4c071647d1b749367a43884dc03003052399744
Paul Campbell <pcampbell@kemitix.net>
2020-10-31 20:02:39 +0000

    docx: create MarkdownDocxRenderer with stub MarkdownDocxConverter

---
+++ binder-docx/src/main/java/net/kemitix/binder/docx/MarkdownDocxConverter.java
@@ -0,0 +1,12 @@
+package net.kemitix.binder.docx;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Collections;
+import java.util.List;
+
+@ApplicationScoped
+public class MarkdownDocxConverter {
+    public List<Object> convert(String markdown) {
+        return Collections.emptyList();//TODO
+    }
+}
