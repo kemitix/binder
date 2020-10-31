2548839b263666e5ee249f3786181c07d43a5d11
Paul Campbell <pcampbell@kemitix.net>
2020-10-31 22:41:14 +0000

    DocumentNodeHandler: added

---
+++ binder-docx/src/main/java/net/kemitix/binder/docx/DocumentNodeHandler.java
@@ -0,0 +1,23 @@
+package net.kemitix.binder.docx;
+
+import com.vladsch.flexmark.util.ast.Document;
+import com.vladsch.flexmark.util.ast.Node;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
+import java.util.stream.Stream;
+
+@ApplicationScoped
+public class DocumentNodeHandler
+        implements NodeHandler{
+
+    @Override
+    public Stream<Node> handle(Node node, List<Object> objects) {
+        return Stream.of(node);
+    }
+
+    @Override
+    public boolean canHandle(Class<? extends Node> aClass) {
+        return Document.class.equals(aClass);
+    }
+}
