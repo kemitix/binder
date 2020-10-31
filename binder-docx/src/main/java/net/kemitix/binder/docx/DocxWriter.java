8b6a2d023f245c9d001145fe529cb893c41a0120
Paul Campbell <pcampbell@kemitix.net>
2020-10-31 19:15:05 +0000

    docx: new ManuscriptWriter and initialise an empty document

---
+++ binder-docx/src/main/java/net/kemitix/binder/docx/DocxWriter.java
@@ -0,0 +1,37 @@
+package net.kemitix.binder.docx;
+
+import net.kemitix.binder.spi.BinderConfig;
+import net.kemitix.binder.spi.ManuscriptWriter;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class DocxWriter
+        implements ManuscriptWriter {
+
+    private final BinderConfig binderConfig;
+    private final DocxManuscript docxManuscript;
+
+    @Inject
+    public DocxWriter(
+            BinderConfig binderConfig,
+            DocxManuscript docxManuscript
+    ) {
+        this.binderConfig = binderConfig;
+        this.docxManuscript = docxManuscript;
+    }
+
+    @Override
+    public void write() {
+        String docxFile = binderConfig.getDocxFile().getAbsolutePath();
+        try {
+            docxManuscript.writeToFile(docxFile);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format(
+                    "Error creating docx file %s: %s",
+                    docxFile, e.getMessage()), e);
+        }
+
+    }
+}
