aa8264c6551689b4c720659eb96735dcc81fd181
Paul Campbell <pcampbell@kemitix.net>
2020-10-31 22:28:32 +0000

    MarkdownDocxConverter: start traversing the document nodes
    No handlers, other than the default IGNORE case.

--- binder-docx/src/main/java/net/kemitix/binder/docx/MarkdownDocxConverter.java
+++ binder-docx/src/main/java/net/kemitix/binder/docx/MarkdownDocxConverter.java
@@ -2,26 +2,73 @@ package net.kemitix.binder.docx;
 
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.ast.Document;
+import com.vladsch.flexmark.util.ast.Node;
+import lombok.extern.java.Log;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+@Log
 @ApplicationScoped
 public class MarkdownDocxConverter {
 
     private final Parser parser;
+    private final Instance<NodeHandler> nodeHandlers;
 
     @Inject
-    public MarkdownDocxConverter(Parser parser) {
+    public MarkdownDocxConverter(
+            Parser parser,
+            Instance<NodeHandler> nodeHandlers
+    ) {
         this.parser = parser;
+        this.nodeHandlers = nodeHandlers;
     }
 
     public List<Object> convert(String markdown) {
         Document document = parser.parse(markdown);
-        //TODO
-        return Collections.emptyList();
+        return accept(document).collect(Collectors.toList());
+    }
+
+    private Stream<Object> accept(Node node) {
+        List<Object> objects = new ArrayList<>();
+        objects.addAll(findHandler(node.getClass())
+                .handle(node, objects)
+                .map(handledNode ->
+                        Stream.of(
+                                handledNode.getFirstChild(),
+                                handledNode.getNext()
+                        )
+                                .filter(Objects::nonNull)
+                                .flatMap(this::accept)).collect(Collectors.toList()));
+        return objects.stream();
+    }
+
+    private NodeHandler findHandler(Class<? extends Node> aClass) {
+        return nodeHandlers.stream()
+                .filter(handler -> handler.canHandle(aClass))
+                .findFirst()
+                .orElseGet(ignoreNodeHandler());
+    }
+
+    private Supplier<NodeHandler> ignoreNodeHandler() {
+        return () -> new NodeHandler() {
+            @Override
+            public Stream<Node> handle(Node node, List<Object> objects) {
+                log.info("Ignoring: %s".formatted(node.getClass().getSimpleName()));
+                return Stream.empty();
+            }
+            @Override
+            public boolean canHandle(Class<? extends Node> aClass) {
+                return true;
+            }
+        };
     }
 
 }
---
+++ binder-docx/src/main/java/net/kemitix/binder/docx/NodeHandler.java
@@ -0,0 +1,13 @@
+package net.kemitix.binder.docx;
+
+import com.vladsch.flexmark.util.ast.Node;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public interface NodeHandler {
+    Stream<Node> handle(Node node, List<Object> objects);
+
+    boolean canHandle(Class<? extends Node> aClass);
+}
