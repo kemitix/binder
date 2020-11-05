22c326a81791092d67e6db7d8b1dab76770bd164
Paul Campbell <pcampbell@kemitix.net>
2020-11-05 14:26:25 +0000

    epub.DocumentEpubNodeHandler: wrap in full html body
    Includes epub namespace declaration

--- binder-epub/src/main/java/net/kemitix/binder/epub/mdconvert/DocumentEpubNodeHandler.java
+++ binder-epub/src/main/java/net/kemitix/binder/epub/mdconvert/DocumentEpubNodeHandler.java
@@ -1,11 +1,28 @@
 package net.kemitix.binder.epub.mdconvert;
 
+import com.vladsch.flexmark.util.ast.Document;
 import net.kemitix.binder.markdown.DocumentNodeHandler;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.stream.Stream;
 
 @Epub
 @ApplicationScoped
 public class DocumentEpubNodeHandler
         implements DocumentNodeHandler<String>, EpubNodeHandler {
+    @Override
+    public Stream<String> documentBody(Document node, Stream<String> content) {
+        return Stream.of(
+                """
+                        <html
+                         xmlns="http://www.w3.org/1999/xhtml"
+                         xmlns:epub="http://www.idpf.org/2007/ops">
+                        <head>
+                        </head>
+                        <body>
+                        %s
+                        </body>
+                        </html>
+                        """.formatted(collect(content)));
+    }
 }
--- binder-markdown/src/main/java/net/kemitix/binder/markdown/DocumentNodeHandler.java
+++ binder-markdown/src/main/java/net/kemitix/binder/markdown/DocumentNodeHandler.java
@@ -3,6 +3,8 @@ package net.kemitix.binder.markdown;
 import com.vladsch.flexmark.util.ast.Document;
 import com.vladsch.flexmark.util.ast.Node;
 
+import java.util.stream.Stream;
+
 public interface DocumentNodeHandler<T>
         extends NodeHandler<T> {
 
@@ -11,4 +13,12 @@ public interface DocumentNodeHandler<T>
         return Document.class;
     }
 
+    @Override
+    default Stream<T> body(Node node, Stream<T> content) {
+        return documentBody((Document) node, content);
+    }
+
+    default Stream<T> documentBody(Document node, Stream<T> content) {
+        return content;
+    }
 }
