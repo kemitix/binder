766e0ad7d9af064d565e6cfdc13117b65cd00232
Paul Campbell <pcampbell@kemitix.net>
2020-11-12 20:00:41 +0000

    epub.mdconvert.SoftLineBreak: represent as a space.

--- binder-epub/src/main/java/net/kemitix/binder/epub/mdconvert/SoftLineBreakEpubNodeHandler.java
+++ binder-epub/src/main/java/net/kemitix/binder/epub/mdconvert/SoftLineBreakEpubNodeHandler.java
@@ -12,7 +12,7 @@ public class SoftLineBreakEpubNodeHandler
     @Override
     public Stream<String> softLineBreakBody() {
         return Stream.of(
-                "<br/>"
+                " "
         );
     }
 }
