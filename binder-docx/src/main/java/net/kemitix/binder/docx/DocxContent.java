5041999cd7cf28e1c14c8a17f0f46bbe33f76b15
Paul Campbell <pcampbell@kemitix.net>
2020-11-15 17:09:50 +0000

    docx.DocxContent: refactoring

--- binder-docx/src/main/java/net/kemitix/binder/docx/DocxContent.java
+++ binder-docx/src/main/java/net/kemitix/binder/docx/DocxContent.java
@@ -2,13 +2,12 @@ package net.kemitix.binder.docx;
 
 import lombok.Getter;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 public class DocxContent {
 
     @Getter
-    private Collection<?> contents = new ArrayList<>();
+    private Collection<?> contents;
 
     public DocxContent(Collection<?> contents) {
         this.contents = contents;
