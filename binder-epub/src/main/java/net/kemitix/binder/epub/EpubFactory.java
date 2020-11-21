8222d013a5ad7c542930d6f7083562e2000e7b09
Paul Campbell <pcampbell@kemitix.net>
2020-11-21 20:25:47 +0000

    epub: add metadata to OPF

--- binder-epub/src/main/java/net/kemitix/binder/epub/EpubFactory.java
+++ binder-epub/src/main/java/net/kemitix/binder/epub/EpubFactory.java
@@ -1,5 +1,6 @@
 package net.kemitix.binder.epub;
 
+import coza.opencollab.epub.creator.impl.OpfCreatorDefault;
 import coza.opencollab.epub.creator.model.Content;
 import coza.opencollab.epub.creator.model.EpubBook;
 import lombok.extern.java.Log;
@@ -7,6 +8,7 @@ import net.kemitix.binder.spi.BinderConfig;
 import net.kemitix.binder.spi.HtmlManuscript;
 import net.kemitix.binder.spi.HtmlSection;
 import net.kemitix.binder.spi.Metadata;
+import net.kemitix.binder.spi.Section;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
@@ -16,6 +18,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Log
 @ApplicationScoped
@@ -43,7 +47,7 @@ public class EpubFactory {
     @ApplicationScoped
     public EpubBook create() {
         Metadata metadata = htmlManuscript.getMetadata();
-        EpubBook epub = createEpub(metadata);
+        EpubBook epub = createEpub(metadata, htmlManuscript.sections());
         epub.addContent(stylesheet);
         epub.addCoverImage(coverImage(metadata.getCover()),
                 "image/jpeg", "cover.jpg");
@@ -67,7 +71,10 @@ public class EpubFactory {
         }
     }
 
-    private EpubBook createEpub(Metadata metadata) {
+    private EpubBook createEpub(
+            Metadata metadata,
+            Stream<HtmlSection> sections
+    ) {
         String language = metadata.getLanguage();
         log.info("Language: " + language);
         String id = Objects.requireNonNull(metadata.getId(), "metadata id");
@@ -77,6 +84,45 @@ public class EpubFactory {
         log.info("Title: " + title);
         String editor = metadata.getEditor();
         log.info("Editor: " + editor);
-        return new EpubBook(language, id, title, editor);
+        EpubBook epubBook = new EpubBook(language, id, title, editor);
+        OpfCreatorDefault opfCreator = ((OpfCreatorDefault) epubBook.getEpubCreator().getOpfCreator());
+        String creators = sections
+                .map(Section::getAuthor)
+                .filter(Objects::nonNull)
+                .map("<dc:creator>%s</dc:creator>"::formatted)
+                .collect(Collectors.joining());
+        opfCreator.setOpfXML("""
+            <?xml version="1.0" encoding="UTF-8"?>
+                <package xmlns="http://www.idpf.org/2007/opf" version="3.0" unique-identifier="uid">
+                    <metadata xmlns:dc="http://purl.org/dc/elements/1.1/">
+                        <dc:identifier id="uid"></dc:identifier>
+                        <dc:title></dc:title>
+                        <dc:language></dc:language>
+                        <dc:description>%6$s</dc:description>
+                        <dc:date>%1$s</dc:date>
+                        <dc:publisher>%2$s</dc:publisher>
+                        <dc:contributor id='editor-id'>%2$s</dc:contributor>
+                        <meta property="role" refines="#editor-id">Editor</meta>
+                        <meta property="belongs-to-collection" id="collection-id">%3$s</meta>
+                        <meta property="collection-type" refines="#collection-id">series</meta>
+                        <meta property="group-position" refines="#collection-id">%4$s</opf:meta>
+                        <meta property="dcterms:modified"></meta>
+                        %5$s
+                    </metadata>
+                    <manifest>
+                    </manifest>
+                    <spine>
+                    </spine>
+                </package>
+                """
+                .formatted(
+                        metadata.getDate(),
+                        metadata.getEditor(),
+                        title,
+                        metadata.getIssue(),
+                        creators,
+                        metadata.getDescription()
+                ));
+        return epubBook;
     }
 }
