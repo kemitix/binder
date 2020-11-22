fdea1bbdad71c3750364cbcb8b9e32deb941acaf
Paul Campbell <pcampbell@kemitix.net>
2020-11-22 15:38:43 +0000

    Add TOC for Kindle (#91)
    * epub: add metadata to OPF
    
    * Bump epub-creator from 1.0.2 to 1.1.0
    
    * epub: Add all metadata
    
    * epub: add table of contents

--- binder-epub/src/main/java/net/kemitix/binder/epub/EpubFactory.java
+++ binder-epub/src/main/java/net/kemitix/binder/epub/EpubFactory.java
@@ -1,12 +1,15 @@
 package net.kemitix.binder.epub;
 
+import coza.opencollab.epub.creator.api.MetadataItem;
 import coza.opencollab.epub.creator.model.Content;
 import coza.opencollab.epub.creator.model.EpubBook;
+import coza.opencollab.epub.creator.model.TocLink;
 import lombok.extern.java.Log;
 import net.kemitix.binder.spi.BinderConfig;
 import net.kemitix.binder.spi.HtmlManuscript;
 import net.kemitix.binder.spi.HtmlSection;
 import net.kemitix.binder.spi.Metadata;
+import net.kemitix.binder.spi.Section;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
@@ -15,7 +18,10 @@ import javax.inject.Named;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 @Log
 @ApplicationScoped
@@ -43,17 +49,40 @@ public class EpubFactory {
     @ApplicationScoped
     public EpubBook create() {
         Metadata metadata = htmlManuscript.getMetadata();
-        EpubBook epub = createEpub(metadata);
+        EpubBook epub = createEpub(metadata, htmlManuscript.sections());
+        epub.setAutoToc(false);
         epub.addContent(stylesheet);
-        epub.addCoverImage(coverImage(metadata.getCover()),
-                "image/jpeg", "cover.jpg");
-        epub.addTextContent("Cover", "cover.html",
-                "<img src=\"cover.jpg\" style=\"height:100%\"/>");
+        Content cover = addCover(metadata, epub);
+        addSections(epub);
+        addTableOfContents(epub, cover);
+        return epub;
+    }
+
+    private void addSections(EpubBook epub) {
         htmlManuscript.sections()
                 .filter(HtmlSection::isEpub)
                 .flatMap(epubSectionRenderer::render)
                 .forEach(epub::addContent);
-        return epub;
+    }
+
+    private Content addCover(Metadata metadata, EpubBook epub) {
+        epub.addCoverImage(coverImage(metadata.getCover()),
+                "image/jpeg", "cover.jpg");
+        Content cover = epub.addTextContent("Cover", "cover.html",
+                "<img src=\"cover.jpg\" style=\"height:100%\"/>");
+        return cover;
+    }
+
+    private void addTableOfContents(EpubBook epub, Content cover) {
+        //TOC
+        List<TocLink> tocLinks = epub.getTocLinks();
+        epub.setTocLinks(tocLinks);
+        tocLinks.add(new TocLink(cover.getHref(), "Cover", ""));
+        htmlManuscript.sections()
+                .filter(HtmlSection::isEpub)
+                .filter(HtmlSection::isToc)
+                .forEach(section ->
+                        tocLinks.add(new TocLink(section.getHref(), section.getTitle(), "")));
     }
 
     private byte[] coverImage(String cover) {
@@ -67,16 +96,38 @@ public class EpubFactory {
         }
     }
 
-    private EpubBook createEpub(Metadata metadata) {
+    private EpubBook createEpub(
+            Metadata metadata,
+            Stream<HtmlSection> sections
+    ) {
         String language = metadata.getLanguage();
         log.info("Language: " + language);
         String id = Objects.requireNonNull(metadata.getId(), "metadata id");
         log.info("Id: " + id);
-        String title = "%s - Issue %d".formatted(
+        String title = "%s Issue %d".formatted(
                 metadata.getTitle(), metadata.getIssue());
         log.info("Title: " + title);
         String editor = metadata.getEditor();
         log.info("Editor: " + editor);
-        return new EpubBook(language, id, title, editor);
+        EpubBook epubBook = new EpubBook(language, id, title, editor);
+        MetadataItem.Builder mib = MetadataItem.builder();
+        var meta = mib.name("meta");
+        var dcCreator = mib.name("dc:creator");
+        var metadataItems = Arrays.asList(
+                mib.name("dc:description").value(metadata.getDescription()),
+                mib.name("dc:contributor").id("editor-id").value(metadata.getEditor()),
+                mib.name("dc:date").value(metadata.getDate()+"T00:00:00+00:00"),
+                mib.name("dc:publisher").value(editor),
+                meta.property("role").refines("#editor-id").value("Editor"),
+                meta.id("collection-id").property("belongs-to-collection").value(metadata.getTitle()),
+                meta.property("collection-type").refines("#collection-id").value("series"),
+                meta.property("group-position").refines("#collection-id").value(Integer.toString(metadata.getIssue()))
+        );
+        metadataItems.forEach(epubBook::addMetadata);
+        sections.map(Section::getAuthor)
+                .filter(Objects::nonNull)
+                .map(dcCreator::value)
+                .forEach(epubBook::addMetadata);
+        return epubBook;
     }
 }
