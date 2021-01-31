e8ee2362b20916e0a5be8bf9b8446dbccdf86898
Paul Campbell <pcampbell@kemitix.net>
2021-01-31 19:56:56 +0000

    epub.FootnoteBlockEpubNodeHandler: add body

--- binder-epub/src/main/java/net/kemitix/binder/epub/mdconvert/FootnoteBlockEpubNodeHandler.java
+++ binder-epub/src/main/java/net/kemitix/binder/epub/mdconvert/FootnoteBlockEpubNodeHandler.java
@@ -4,6 +4,7 @@ import net.kemitix.binder.markdown.FootnoteBlockNodeHandler;
 import net.kemitix.binder.markdown.FootnoteBody;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Epub
@@ -12,6 +13,14 @@ public class FootnoteBlockEpubNodeHandler
         implements FootnoteBlockNodeHandler<String>, EpubNodeHandler {
     @Override
     public Stream<String> footnoteBlockBody(FootnoteBody<String> footnoteBody) {
-        return Stream.empty();
+        return Stream.of(
+                """
+                        <p>%1$s</p>
+                        <blockquote>
+                        """.formatted(footnoteBody.getOridinal()),
+                footnoteBody.getContent().collect(Collectors.joining())
+                .replaceAll(" ~PARA~ ", "</p><p>"),
+                "</blockquote>"
+        );
     }
 }
