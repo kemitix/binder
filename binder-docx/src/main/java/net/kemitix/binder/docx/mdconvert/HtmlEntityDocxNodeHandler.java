1e06396482c8bc3feac3a9aa696a56abd67ed523
Paul Campbell <pcampbell@kemitix.net>
2021-08-10 12:40:58 +0100

    HtmlEntityDocxNodeHandler: added

---
+++ binder-docx/src/main/java/net/kemitix/binder/docx/mdconvert/HtmlEntityDocxNodeHandler.java
@@ -0,0 +1,13 @@
+package net.kemitix.binder.docx.mdconvert;
+
+import net.kemitix.binder.docx.DocxRenderHolder;
+import net.kemitix.binder.markdown.HtmlEntityNodeHandler;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Docx
+@ApplicationScoped
+public class HtmlEntityDocxNodeHandler
+        implements HtmlEntityNodeHandler<String, DocxRenderHolder> {
+
+}
